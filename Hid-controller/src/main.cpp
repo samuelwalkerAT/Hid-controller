75ca48de157ea4bc554df095712a4346b8651bf8
Samuel Maurice Walker <samuelwalkerat@gmail.com>
2024-05-26 21:58:36 -0400

    added switch

--- Hid-controller/src/main.cpp
+++ Hid-controller/src/main.cpp
@@ -1,103 +1,190 @@
-#include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
+#include <Wire.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
-#include <Wire.h>
-#include "mouse.h"
-#include "Keyboard.h"
-#include "Adafruit_CircuitPlayground.h"
-// put function declarations here:
-// initalize mpu6050 and neopixel
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, 6, NEO_GRB + NEO_KHZ800);
+#include <Mouse.h>
+#include <Keyboard.h>
+#include <Adafruit_CircuitPlayground.h>
 Adafruit_MPU6050 mpu;
-// set up mouse and keyboard
-// initalize playground express native button
-bool active = false;
-int rightButton = 19;
-int leftButton = 4;
-int activeswitch = 21;
-int readingx;
-int readingy;
+
+enum MovementType
+{
+  NO_MOVEMENT,
+  PAN_RIGHT,
+  PAN_LEFT,
+  PAN_UP,
+  PAN_DOWN,
+  ROTATE_CLOCKWISE,
+  ROTATE_COUNTERCLOCKWISE,
+  ZOOM_IN,
+  ZOOM_OUT
+};
+
 void setup()
 {
-  // pinMode(activeswitch, INPUT);
-  Serial.begin(9600);
-  delay(1000);
-  Serial.println("Started");
-  // set up mpu6050 and neopixel
-  pixels.begin();
-  // pinMode(rightButton, INPUT_PULLUP);
-  Wire.begin();
-
-  // set up mouse and keyboard
+  Adafruit_CircuitPlayground CircuitPlayground;
+  // Initialize serial communication for debugging purposes
+  Serial.begin(115200);
+  // initiize mouse and keyboard
   Mouse.begin();
-  if (mpu.begin())
+  Keyboard.begin();
+
+  // Initialize MPU6050
+  if (!mpu.begin())
   {
-    Serial.println("mpu begun");
-    mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
-    mpu.setGyroRange(MPU6050_RANGE_2000_DEG);
-    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-    delay(1000);
+    Serial.println("Failed to find MPU6050 chip");
+    while (1)
+    {
+      delay(10);
+    }
   }
-  else if (!mpu.begin())
+
+  // Set accelerometer range and filter bandwidth
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+}
+MovementType determineMovement(sensors_event_t a, sensors_event_t g)
+{
+  // Define thresholds for actions
+  const float panThreshold = 5.0;
+  const float rotateThreshold = 20.0;
+  const float zoomThreshold = 15.0;
+
+  // Determine action based on MPU6050 data
+  if (abs(a.acceleration.x) > panThreshold)
   {
-    Serial.println("mpu not begun");
-  } // map accelerometer to mouse movement
+    return (a.acceleration.x > 0) ? PAN_RIGHT : PAN_LEFT;
+  }
+  else if (abs(a.acceleration.y > panThreshold))
+  {
 
-  // initalize playground express native button
-
-  pixels.setPixelColor(0, 255, 0, 0);
-  pixels.show();
-
-  delay(1000);
+    return (a.acceleration.y > 0) ? PAN_UP : PAN_DOWN;
+  }
+  else if (abs(g.gyro.y) > rotateThreshold)
+  {
+    return (g.gyro.y > 0) ? ROTATE_CLOCKWISE : ROTATE_COUNTERCLOCKWISE;
+  }
+  else if (abs(a.acceleration.z) > zoomThreshold)
+  {
+    return (a.acceleration.z > 0) ? ZOOM_IN : ZOOM_OUT;
+  }
+  else
+  {
+    return NO_MOVEMENT;
+  }
 }
 
+void panRight()
+{
+  Serial.println("pan right");
+  Mouse.press(MOUSE_MIDDLE);
+  Mouse.move(10, 0, 0); // Move mouse right
+  delay(100);
+  Mouse.release(MOUSE_MIDDLE);
+}
+
+void panLeft()
+{
+  Serial.println("pan left");
+  Mouse.press(MOUSE_MIDDLE);
+  Mouse.move(-10, 0, 0); // Move mouse left
+  delay(100);
+  Mouse.release(MOUSE_MIDDLE);
+}
+void panUp()
+{
+  // pan up implimentation
+  Serial.println("pan up");
+  Mouse.press(MOUSE_MIDDLE);
+  Mouse.move(0, 10, 0); // Move mouse right
+  delay(100);
+  Mouse.release(MOUSE_MIDDLE);
+}
+void panDown()
+{
+  // pan down
+  Serial.println("pan Down");
+  Mouse.press(MOUSE_MIDDLE);
+  Mouse.move(0, -10, 0); // Move mouse right
+  delay(100);
+  Mouse.release(MOUSE_MIDDLE);
+}
+void rotateClockwise()
+{
+  Serial.println("rotate clockwise ");
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Mouse.press(MOUSE_RIGHT);
+  Mouse.move(10, 0, 0); // Move mouse right
+  delay(100);
+  Mouse.release(MOUSE_RIGHT);
+  Keyboard.release(KEY_LEFT_SHIFT);
+}
+
+void rotateCounterClockwise()
+{
+  Serial.println("rotate counter ");
+  Keyboard.press(KEY_RIGHT_SHIFT);
+  Mouse.press(MOUSE_RIGHT);
+  Mouse.move(-10, 0, 0); // Move mouse left
+  delay(100);
+  Mouse.release(MOUSE_RIGHT);
+  Keyboard.release(KEY_RIGHT_SHIFT);
+}
+
+void zoomIn()
+{
+  Serial.println("zoom in");
+  Keyboard.press(KEY_RIGHT_CTRL);
+  Mouse.move(0, 0, 1); // Scroll up
+  delay(100);
+  Keyboard.release(KEY_RIGHT_CTRL);
+}
+
+void zoomOut()
+{
+  Serial.println("zoom out ");
+  Keyboard.press(KEY_RIGHT_CTRL);
+  Mouse.move(0, 0, -1); // Scroll down
+  delay(100);
+  Keyboard.release(KEY_RIGHT_CTRL);
+}
+void handleMovement(MovementType movement)
+{
+  switch (movement)
+  {
+  case PAN_RIGHT:
+    panRight();
+    break;
+  case PAN_LEFT:
+    panLeft();
+    break;
+  case ROTATE_CLOCKWISE:
+    rotateClockwise();
+    break;
+  case ROTATE_COUNTERCLOCKWISE:
+    rotateCounterClockwise();
+    break;
+  case ZOOM_IN:
+    zoomIn();
+    break;
+  case ZOOM_OUT:
+    zoomOut();
+    break;
+  case NO_MOVEMENT:
+    // No action needed
+    break;
+  }
+}
 void loop()
 {
-
-  // active = digitalRead(activeswitch);
-  //  if(!active){
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-  readingx = map(g.gyro.x, -120, 120, -1000, 1000);
-  readingy = map(a.acceleration.y, -8, 8, -100, 100);
-  // Serial.print("x,");
-  /* Print out the values */ Serial.print("AccelX:");
-  Serial.print(a.acceleration.x);
-  Serial.print(",");
-  Serial.print("AccelY:");
-  Serial.print(a.acceleration.y);
-  Serial.print(",");
-  Serial.print("AccelZ:");
-  Serial.print(a.acceleration.z);
-  Serial.print(", ");
-  Serial.print("GyroX:");
-  Serial.print(g.gyro.x);
-  Serial.print(",");
-  Serial.print("GyroY:");
-  Serial.print(g.gyro.y);
-  Serial.print(",");
-  Serial.print("GyroZ:");
-  Serial.print(g.gyro.z);
-  Serial.println("");
-  // take mpu readings and send mouse movement wait for movement greater than offset
-  int offset = 1;
-
-  Mouse.move(a.acceleration.x, a.acceleration.y);
-
-  // Mouse.release();
-  delay(10);
-
-  // }
-}
-
-void fusion360(sensors_event_t a, sensors_event_t g)
-{
-  // switch case to handle move in fusion 360 3 cases for pan rotate and zoom corrisponding to fusion mouse click movments
-  switch (a)
+  // if circuit playground switch is on use device as mouse
+  if (CircuitPlayground.slideSwitch())
   {
-  case break;
-      gcjgcjg default:
-    break;
+    Serial.println("hid active");
+    MovementType movement = determineMovement(a, g);
+    handleMovement(movement);
+    delay(100); // Adjust delay as needed
   }
 }
