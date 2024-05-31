deaa446bca578f98a548d68c84e833b2d7bc1d8a
Samuel Maurice Walker <samuelwalkerat@gmail.com>
2024-05-31 19:47:18 -0400

    added hall effect setup

--- Hid-controller/src/main.cpp
+++ Hid-controller/src/main.cpp
@@ -9,6 +9,9 @@ Adafruit_MPU6050 mpu;
 Adafruit_CircuitPlayground Play;
 bool debug = 0; // set true for debug
 
+int rotX = A3;
+int rotY = A3;
+
 enum MovementType
 {
   NO_MOVEMENT,
@@ -18,12 +21,16 @@ enum MovementType
   PAN_DOWN,
   ROTATE_CLOCKWISE,
   ROTATE_COUNTERCLOCKWISE,
+  ROTATE_UP,
+  ROTATE_DOWN,
   ZOOM_IN,
   ZOOM_OUT
 };
 
 void setup()
 {
+  pinMode(rotX, INPUT);
+  pinMode(rotY, INPUT);
   Play.begin();
   // Initialize serial communication for debugging purposes
   Serial.begin(115200);
@@ -49,9 +56,9 @@ void setup()
 MovementType determineMovement(sensors_event_t a, sensors_event_t g)
 {
   // Define thresholds for actions
-  const float panThreshold = 10.0;
-  const float rotateThreshold = 5.0;
-  const float zoomThreshold = 15.0;
+  const float panThreshold = 3.0;
+  const float rotateThreshold = 523.0;
+  const float zoomThreshold = 12.0;
 
   // Determine action based on MPU6050 data
   if (abs(a.acceleration.x) > panThreshold)
@@ -63,9 +70,22 @@ MovementType determineMovement(sensors_event_t a, sensors_event_t g)
 
     return (a.acceleration.y > 0) ? PAN_UP : PAN_DOWN;
   }
-  else if (abs(g.gyro.y) > rotateThreshold)
+  else if (analogRead(rotX) > rotateThreshold + 50)
   {
-    return (g.gyro.y > 0) ? ROTATE_CLOCKWISE : ROTATE_COUNTERCLOCKWISE;
+    // return ROTATE_COUNTERCLOCKWISE;
+  }
+  else if (analogRead(rotX) < rotateThreshold - 50)
+  {
+
+    // return ROTATE_CLOCKWISE;
+  }
+  else if (analogRead(rotY) > rotateThreshold + 50)
+  {
+    return ROTATE_UP;
+  }
+  else if (analogRead(rotY) < rotateThreshold - 50)
+  {
+    return ROTATE_DOWN;
   }
   else if (abs(a.acceleration.z) > zoomThreshold)
   {
@@ -132,7 +152,7 @@ void rotateClockwise()
     Keyboard.press(KEY_LEFT_SHIFT);
     Mouse.press(MOUSE_MIDDLE);
     Mouse.move(10, 0, 0); // Move mouse right
-    delay(100);
+    delay(10);
     Mouse.release(MOUSE_MIDDLE);
     Keyboard.release(KEY_LEFT_SHIFT);
   }
@@ -146,7 +166,34 @@ void rotateCounterClockwise()
     Keyboard.press(KEY_RIGHT_SHIFT);
     Mouse.press(MOUSE_MIDDLE);
     Mouse.move(-10, 0, 0); // Move mouse left
-    delay(100);
+    delay(10);
+    Mouse.release(MOUSE_MIDDLE);
+    Keyboard.release(KEY_RIGHT_SHIFT);
+  }
+}
+void rotateUp()
+{
+  Serial.println("rotate up");
+  if (!debug)
+  {
+
+    Keyboard.press(KEY_RIGHT_SHIFT);
+    Mouse.press(MOUSE_MIDDLE);
+    Mouse.move(0, 10, 0); // Move mouse left
+    delay(10);
+    Mouse.release(MOUSE_MIDDLE);
+    Keyboard.release(KEY_RIGHT_SHIFT);
+  }
+}
+void rotateDown()
+{
+  Serial.println("rotate down");
+  if (!debug)
+  {
+    Keyboard.press(KEY_RIGHT_SHIFT);
+    Mouse.press(MOUSE_MIDDLE);
+    Mouse.move(0, -10, 0); // Move mouse left
+    delay(10);
     Mouse.release(MOUSE_MIDDLE);
     Keyboard.release(KEY_RIGHT_SHIFT);
   }
@@ -158,10 +205,10 @@ void zoomIn()
   Serial.println("zoom in");
   if (!debug)
   {
-    Keyboard.press(KEY_RIGHT_CTRL);
+    // Keyboard.press(KEY_RIGHT_CTRL);
     Mouse.move(0, 0, 1); // Scroll up
     delay(100);
-    Keyboard.release(KEY_RIGHT_CTRL);
+    // Keyboard.release(KEY_RIGHT_CTRL);
   }
 }
 
@@ -170,10 +217,10 @@ void zoomOut()
   Serial.println("zoom out ");
   if (!debug)
   {
-    Keyboard.press(KEY_RIGHT_CTRL);
+    // Keyboard.press(KEY_RIGHT_CTRL);
     Mouse.move(0, 0, -1); // Scroll down
     delay(100);
-    Keyboard.release(KEY_RIGHT_CTRL);
+    // Keyboard.release(KEY_RIGHT_CTRL);
   }
 }
 void handleMovement(MovementType movement)
@@ -199,6 +246,12 @@ void handleMovement(MovementType movement)
   case ROTATE_COUNTERCLOCKWISE:
     rotateCounterClockwise();
     break;
+  case ROTATE_UP:
+    rotateUp();
+    break;
+  case ROTATE_DOWN:
+    rotateDown();
+    break;
   case ZOOM_IN:
     zoomIn();
     break;
@@ -215,6 +268,9 @@ void loop()
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
   // if circuit playground switch is on use device as mouse
+  Serial.print("rotx:");
+  Serial.println(analogRead(rotX));
+  // delay(1000);
   if (!Play.slideSwitch())
   {
     Mouse.begin();
@@ -228,9 +284,9 @@ void loop()
   else
   {
     Mouse.end();
-    Serial.print("hid not active");
+    // Serial.print("hid not active");
     // Serial.print("debug");
-    Serial.println(debug);
+    // Serial.println(debug);
     delay(100);
   }
 }
